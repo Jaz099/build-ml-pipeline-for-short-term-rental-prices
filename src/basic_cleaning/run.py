0b51c2c719aad52d488dc0aed703f608d3adeeb7
Jaz099 <yasmine.benayed@etudiant-enit.utm.tn>
2024-01-31 15:09:41 +0100

    fix: Add the longitude and latitude restraints

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -29,6 +29,10 @@ def go(args):
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
 
+    # Add the longitude and latitude restraints
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     #Convert the column last_review to datetime
     logger.info("Convert the column last_review to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
