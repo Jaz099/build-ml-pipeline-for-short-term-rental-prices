92379c1bc8d6a7fa4cd394da02ff16e00e1ad436
Jaz099 <yasmine.benayed@etudiant-enit.utm.tn>
2024-02-01 09:20:19 +0100

    fix: add Restraint the logitude and the latitude

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -29,10 +29,14 @@ def go(args):
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
 
-    #Convert the column last_review to datetime
+    # Convert the column last_review to datetime
     logger.info("Convert the column last_review to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    # Restraint the logitude and the latitude
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+    
     # Save the resulting dataframe into a csv file
     logger.info("Save the resulting dataframe into a csv file")
     df.to_csv("clean_sample.csv", index=False)
